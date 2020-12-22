15ec803ae48f16d363ac0d67b6540b4a4cdcb128
tdytaylor <949722633@qq.com>
2020-12-22 11:41:31 +0800

    项目初始化

---
+++ src/main/java/org/arch/eshop/EshopInventoryApplication.java
@@ -0,0 +1,16 @@
+package org.arch.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tdytaylor
+ */
+@SpringBootApplication
+public class EshopInventoryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EshopInventoryApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/arch/eshop/EshopInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arch.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopInventoryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
